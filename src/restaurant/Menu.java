3722280a96d3a4eeb0b6f2651ebf11c602c146b9
Michaela Goldsmith <megold12@yahoo.com>
2021-01-25 20:46:09 -0600

    studio restaurant pt 1

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,15 @@
+package restaurant;
+
+import java.util.Date;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public static void main(String[]args){
+
+    public class Menu {
+        Date lastUpdated;
+
+    }
+}
+
