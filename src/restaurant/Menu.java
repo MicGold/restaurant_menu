ca6565e1396111b196a1bc95d1a48ac103f81468
Michaela Goldsmith <megold12@yahoo.com>
2021-01-28 19:47:18 -0600

    Restaurant menu init

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,15 +1,50 @@
 package restaurant;
 
+import java.lang.reflect.Array;
 import java.util.Date;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public static void main(String[]args){
+public class Menu {
+    private Date lastUpdated;
+    private ArrayList<MenuItem> items;
+    private HashMap<String, ArrayList<MenuItem>> categoryToItemsMap = new HashMap<>();
 
-    public class Menu {
-        Date lastUpdated;
+    public Menu(ArrayList<MenuItem> items){
+        setItems(items);
+    }
 
+    public Menu(Date d, ArrayList<MenuItem> items) {
+        setItems(items);
+        this.lastUpdated = d;
+    }
+
+    public void setItems(ArrayList<MenuItem> items) {
+        this.items = items;
+        this.lastUpdated = new Date();
+    }
+
+    public Date getLastUpdated() {
+        return this.lastUpdated;
+    }
+
+    public String getMenuString() {
+        String str = "";
+        for(MenuItem item : this.items){
+            str += item.getCategory() + " (" + item.getPrice() + ")" + "-" + item.getDescription();
+        }
+        return str;
+    }
+
+    public String printMenu() {
+        String str = "APPETIZERS: ";
+        for(MenuItem item : this.items){
+            if(item.getCategory() = "Appetizer"){
+                str += item.getCategory() + " (" + item.getPrice() + ")" + "-" + item.getDescription();
+            }
+        }
+        return str;
     }
 }
 
